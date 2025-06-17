e207bf24d2e0fe8de6a38703f72f0b79d999a1fb
awtnb <arikaebanataw@gmail.com>
2025-06-17 13:06:36 +0900

    update setup ps1

--- set-startmenu.ps1
+++ set-startmenu.ps1
@@ -1,16 +1,32 @@
-$vscode = (Get-Command "Code").Source | Split-Path -Parent | Split-Path -Parent | Join-Path -ChildPath "code.exe"
-if (Test-Path $vscode) {
+function New-StartupShortcut {
+    param (
+        [string]$shortcutName,
+        [string]$targetPath,
+        [string]$argument = ""
+    )
     $wsShell = New-Object -ComObject WScript.Shell
     $startMenu = $env:USERPROFILE | Join-Path -ChildPath "AppData\Roaming\Microsoft\Windows\Start Menu\Programs"
-    $shortcutName = "{0}-on-vscode.lnk" -f ($PSScriptRoot | Split-Path -Leaf)
+    if (-not $shortcutName.EndsWith(".lnk")) {
+        $shortcutName = $shortcutName + ".lnk"
+    }
     $shortcutPath = $startMenu | Join-Path -ChildPath $shortcutName
     $shortcut = $wsShell.CreateShortcut($shortcutPath)
-    $shortcut.TargetPath = $vscode
-    $shortcut.Arguments = '"{0}"' -f $PSScriptRoot
+    $shortcut.TargetPath = $targetPath
+    if ($argument.Length -gt 0) {
+        $shortcut.Arguments = '"{0}"' -f $argument
+    }
     $shortcut.Save()
-    "Created shortcut on start menu: {0}" -f $shortcutPath | Write-Host -ForegroundColor Blue
+}
+
+$vscode = (Get-Command "Code").Source | Split-Path -Parent | Split-Path -Parent | Join-Path -ChildPath "code.exe"
+if (Test-Path $vscode) {
+    $shortcutName = "{0}-on-vscode.lnk" -f ($PSScriptRoot | Split-Path -Leaf)
+    New-StartupShortcut -shortcutName $shortcutName -targetPath $vscode -argument ('"{0}"' -f $PSScriptRoot)
+    "Created shortcut on start menu: {0}" -f $shortcutName | Write-Host -ForegroundColor Blue
 }
 else {
     "Cannot find VSCode on '{0}'" -f $vscode | Write-Host -ForegroundColor Magenta
 }
 
+New-StartupShortcut -shortcutName "CorvusSKK-config.lnk" -targetPath "C:\Windows\System32\IME\IMCRVSKK\imcrvcnf.exe"
+"Created shortcut on start menu: {0}" -f "CorvusSKK-config.lnk" | Write-Host -ForegroundColor Blue
